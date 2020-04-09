82e1c75535921e53652c8ca8f2dc5f70dcb55636
schneems <richard.schneeman+foo@gmail.com>
2020-04-09 15:27:13 -0500

    Re-fix focused tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,11 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "rate_throttle_client"
 
-require "minitest"
-# https://github.com/seattlerb/minitest/pull/683#issuecomment-611302188
-Minitest.run
 # require "minitest/autorun"
+
+# https://github.com/seattlerb/minitest/issues/844
+require "minitest"
+Minitest.run
 
 require 'rate_throttle_client/demo'
 
