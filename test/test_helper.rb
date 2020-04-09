fd75c42da11e8756357fa8adb5f8fea87b64e717
schneems <richard.schneeman+foo@gmail.com>
2020-04-09 15:20:52 -0500

    Re-fix test-output behavior

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,10 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "rate_throttle_client"
 
-# require "minitest"
+require "minitest"
 # https://github.com/seattlerb/minitest/pull/683#issuecomment-611302188
-# Minitest.run
-require "minitest/autorun"
+Minitest.run
+# require "minitest/autorun"
 
 require 'rate_throttle_client/demo'
 
