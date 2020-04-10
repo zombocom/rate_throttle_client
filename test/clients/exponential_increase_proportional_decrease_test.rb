3b87dd5319cb621c37fb8defc4c6694b6d16064e
schneems <richard.schneeman+foo@gmail.com>
2020-04-10 16:11:58 -0500

    Proportional tests

--- test/clients/exponential_increase_proportional_decrease_test.rb
+++ test/clients/exponential_increase_proportional_decrease_test.rb
@@ -10,5 +10,26 @@ module RateThrottleClient
     def test_kwargs
       assert 42, @klass.new(decrease_divisor: 42).decrease_divisor
     end
+
+    def test_proportional_effect_on_sleep
+      client_200_start = 200
+      client_200 = @klass.new(start_sleep_for: client_200_start)
+      def client_200.sleep(value); end
+      client_200.call do
+        FakeResponse.new
+      end
+
+      client_400_start = 400
+      client_400 = @klass.new(start_sleep_for: client_400_start)
+      def client_400.sleep(value); end
+      client_400.call do
+        FakeResponse.new
+      end
+
+      diff_200 = client_200_start - client_200.sleep_for
+      diff_400 = client_400_start - client_400.sleep_for
+
+      assert_in_delta(2 * diff_200, diff_400, 0.001, "Expected a 2x higher sleep value to decrease 2x faster (proportionally)")
+    end
   end
 end
